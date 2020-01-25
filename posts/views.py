55210c22724534906f5feb819c4d8243da1d78a8
ing <inyoman_gurnitha@yahoo.com>
2020-01-25 22:15:44 +0700

    Part 2: 3. Display latest posts

--- posts/views.py
+++ posts/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 from .models import Post 
 
-# part 1 to display all posts
+# # part 1 to display all posts
+# def index(request):
+#     # query/get all post objects, filter by featured
+#     queryset = Post.objects.filter(featured=True)
+#     context = {
+#         'object_list' : queryset
+#     }
+#     return render(request, 'index.html', context)
+
+# part 2 to display all posts and latest posts
 def index(request):
     # query/get all post objects, filter by featured
-    queryset = Post.objects.filter(featured=True)
+    # queryset = Post.objects.filter(featured=True)
+    featured = Post.objects.filter(featured=True)
+    latest   = Post.objects.order_by('-timestamp')[0:3]
+
     context = {
-        'object_list' : queryset
+        'object_list' : featured,
+        'latest'      : latest
     }
+
     return render(request, 'index.html', context)
 
 def blog(request):
