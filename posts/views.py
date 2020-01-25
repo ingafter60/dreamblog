2a93fec00842aa2d27ff48d6b6653f99f7375499
ing <inyoman_gurnitha@yahoo.com>
2020-01-25 22:47:03 +0700

    Part 2: 4. FINAL, Subscribe to Newsletter

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from .models import Post 
+from marketing.models import Signup
 
 # # part 1 to display all posts
 # def index(request):
@@ -11,18 +12,41 @@ from .models import Post
 #     }
 #     return render(request, 'index.html', context)
 
-# part 2 to display all posts and latest posts
+# # part 2 to display all posts and latest posts
+# def index(request):
+#     # query/get all post objects, filter by featured
+#     featured = Post.objects.filter(featured=True)
+#     latest   = Post.objects.order_by('-timestamp')[0:3]
+
+#     context = {
+#         'object_list' : featured,
+#         'latest'      : latest
+#     }
+
+#     return render(request, 'index.html', context)
+
+# part 3 to display all posts and latest posts
 def index(request):
     # query/get all post objects, filter by featured
-    # queryset = Post.objects.filter(featured=True)
     featured = Post.objects.filter(featured=True)
     latest   = Post.objects.order_by('-timestamp')[0:3]
+
+    # Subscribe to Newsletter
+    # get email from the request
+    if request.method == "POST":
+        email = request.POST["email"]
+        # get the signup object
+        new_signup = Signup()
+        # use the email to singup
+        new_signup.email = email
+        # save the email
+        new_signup.save()
 
     context = {
         'object_list' : featured,
         'latest'      : latest
     }
-
+    
     return render(request, 'index.html', context)
 
 def blog(request):
